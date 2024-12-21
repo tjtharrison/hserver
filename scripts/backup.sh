fc5a36a3f98518bc669d18be093b5433b2be0616
tjtharrison <gh@tjth.co>
2024-12-21 17:03:48 +0000

    fix: Fixed script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 # Create backup dir if it doesn't exist
-sudo mkdir /mnt/backup
+backup_dir="/mnt/backup"
+source_dir="/mnt/nfs"
 
-# Generate list of dirs
-exclude_patterns=("archive*" "*db-storage*" "*loki*" "*prometheus*" "*grafana*")
-exclude_args=("${exclude_patterns[@]/#/-not -name }")
-echo "find /mnt/nfs/ -maxdepth 1 -mindepth 1 -type d \"${exclude_args[@]}\""
-dirs=$(find /mnt/nfs/ -maxdepth 1 -mindepth 1 -type d \"${exclude_args[@]}\")
+# Generate list of dirs with exclude patterns
+dirs=$(find $source_dir -maxdepth 1 -mindepth 1 -type d -not -name "archive*" -not -name "*db-storage*" -not -name "*loki*" -not -name "*prometheus*" -not -name "*grafana*")
+
+# Create backup dir if it doesn't exist
+if [ ! -d $backup_dir ]; then
+  mkdir $backup_dir
+fi
 
 # Backup each dir
 for dir in $dirs; do
   dir_name=$(basename $dir)
   today_date=$(date +%F)
-  sudo tar -czf /mnt/backup/$today_date_$dir_name.tar.gz $dir
+  sudo tar -czf $backup_dir/$today_date-$dir_name.tar.gz $dir
 done
 
 # Remove old backups
-find /mnt/backup/ -type f -mtime +30 -exec rm {} \;
+# find $backup_dir -type f -mtime +30 -exec rm {} \;
 
 # Copy backup to NAS
-rsync -avh /mnt/backup root@192.168.0.200:/data/server-backups/hserver
+# rsync -avh /mnt/backup root@192.168.0.200:/data/server-backups/hserver
